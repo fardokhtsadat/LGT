991c8930ef7ab11bca81a02b2e35b92925abc26a
Fardokhtsadat Mohammadi <fardokht.fm@gmail.com>
2020-09-15 11:53:33 +0200

    Update extract_acc_evalue.py

--- extract_acc_evalue.py
+++ extract_acc_evalue.py
@@ -1,17 +1,20 @@
 import pandas as pd
+from multiprocessing import Pool
+import numpy as np
+from math import nan
 import os
+import glob
+import functools
 
-# parse_fasta_headers() parses the headers from a fasta file and stores it in a file. parse_fasta_headers() gets name of a fasta file as input and creates 
+# parse_fasta_headers() parses the headers from a fasta file and stores it in a file. parse_fasta_headers() gets name of a fasta file as input and creates
 # a file containg the headers. for example parse_fasta_headers('OG0000000.fa') creates a file called 'headers_OG0000000' with the headers in 'OG0000000.fa'.
-
 def parse_fasta_headers(afile):
     import os
     os.system("grep -e '>' %s >> headers_%s" %(afile, afile.split('.', 1)[0]))
     os.system("sed -i 's/>//g' headers_%s" %(afile.split('.', 1)[0]))
 
-    
-# modify_string(x) makes sure the headers parsed from fasta files are consistent with the qseqids in pickled data frames. modify_string(x) gets the name of a 
-# file containg the parsed headers from a fasta file, and makes modifications in the names if needed. In this example, modify_string('headers_OG0000000') gets 
+# modify_string(x) makes sure the headers parsed from fasta files are consistent with the qseqids in pickled data frames. modify_string(x) gets the name of a
+# file containg the parsed headers from a fasta file, and makes modifications in the names if needed. In this example, modify_string('headers_OG0000000') gets
 # the file 'headers_OG0000000' and modifies the headers in this file if needed.
 def modify_string(x):
     with open(x) as f:
@@ -27,28 +30,31 @@ def modify_string(x):
 
 
 # First, search_pkl_df() uses the result from modify_string(x) and groups the headers based on the species they belong to.
-# this makes the searching of related qseqids in the pickled data frames faster and more efficient since pickled data frame for the same species are not opened 
+# this makes the searching of related qseqids in the pickled data frames faster and more efficient since pickled data frame for the same species are not opened
 # and closed constantly. Next, search_pkl_df(x) searches for the qseqids in the pickled data frames and create a csv file.
-def search_pkl_df(content):
-    import pandas as pd
+def get_species(content):
     species_list = ['BOT', 'CONGO', 'DAR', 'GYROMONAS', 'Hexamita', 'IT1', 'MACHU_PICCU', 'MIS2C', 'PIG', 'SOOS4','TRIMITUS', 'VLADA7']
-    species = {'BOT': [], 'CONGO': [], 'DAR': [], 'GYROMONAS': [], 'Hexamita': [], 'IT1': [], 'MACHU_PICCU': [], 'MIS2C': [], 'PIG': [], 'SOOS4': [], 'TRIMITUS': [], 'VLADA7': []}
+    species = {'BOT': [], 'CONGO': [], 'DAR': [], 'GYROMONAS': [], 'Hexamita': [], 'IT1': [], 'MACHU_PICCU': [],'MIS2C': [], 'PIG': [], 'SOOS4': [], 'TRIMITUS': [], 'VLADA7': []}
     for i in range(len(content)):
         if content[i].split('_')[0] in species_list:
             element = content[i].split('_')[0]
             species.setdefault(element, [])
             species[element].append(content[i])
         species = {k: v for k, v in species.items() if v}
-    for j in species:
-        species[j] = list(set(species[j]))
+    return species
+
+
+#the following function retrives info from pickle dataframe for an orthogroup
+def search_pkl_df(x):
     empty_df = pd.DataFrame()
-    for z in species:
-        pickle_df = '%s_EUK_df.pkl' % (z)
+    for i in x:
+        pickle_df = '%s_EUK_df.pkl' % (i)
         df = pd.read_pickle(pickle_df)
-        for h in range(len(species[z])):
-            empty_df = empty_df.append(df.loc[df['qseqid'] == species[z][h]], ignore_index=True)
-    empty_df.to_csv('run_result.csv')
-    
+        for j in range(len(x[i])):
+            empty_df = empty_df.append(df.loc[df['qseqid'] == x[i][j]], ignore_index=True)
+    #empty_df.to_csv('run_result.csv')
+    return empty_df
+
 # remove_duplicate_accession removes duplicate sseqids from the result obtained from search_pkl_df:
 def remove_duplicate_accession(x, df):
     EUK_df = pd.DataFrame()
@@ -66,20 +72,32 @@ def remove_duplicate_accession(x, df):
 
 def main(df):
     import multiprocessing as mp
-    import functools
-    pool = mp.Pool(processes=50)
+    pool = mp.Pool(processes=50) #50 processors are used to speed up the process
     ans = pool.map(functools.partial(remove_duplicate_accession, df=df), list(set(df['qseqid'])))
     return ans
 
-#--
-# edit_acc() removes the version number from an accession number. 
+def get_hitproportion_meaneval(df):
+    unique_qseqid = len(set(df['qseqid']))
+    # get occurance of each accession number
+    count = df['sseqid'].value_counts()
+    count = count.to_frame()
+    # get the mean value for each accession number
+    # mean = df.groupby(['col1']).agg([np.average])
+    mean = df.groupby('sseqid').mean()
+    joined_df = pd.concat([count, mean], axis=1)  # concat the two dataframes
+    joined_df.reset_index(inplace=True)  # reset indicies
+    joined_df.columns = ['sseqid', 'occurrence', 'mean_eval']
+    joined_df['occurrence'] = joined_df['occurrence'].div(unique_qseqid)
+    #joined_df.to_csv("acc_hitprop_meaneval.csv", sep='\t', index=False)
+    return joined_df
+
+#--------------get taxid
 def edit_acc(alist):
     edited_acc = []
     for i in alist:
         edited_acc.append(i.split('.', 1)[0])
     return edited_acc
 
-# get_taxid() gets the taxon id for each accession number.
 def get_taxid(x):
     acc_taxid = {}
     result = os.popen("blastdbcmd -entry %s -outfmt '%%T' -db /Data/nr_old/nr" % (x)).read() #try batch entry
@@ -90,28 +108,117 @@ def get_taxid(x):
         acc_taxid[x] = ''.join([int(s) for s in result.split() if s.isdigit()])
     return acc_taxid
 
+#----------get taxonomic rank
+def get_taxonomic_rank(afile):
+    import os
+    result = os.popen("epost -db taxonomy -input %s | efetch -format xml | xtract -pattern Taxon -first TaxId -block \"LineageEx/Taxon\" -unless Rank -equals \"no rank\" -sep \'\t\' -element Rank,ScientificName" %(afile)).read()
+    result = result.split('\n')
+    for i in range(len(result)):
+        result[i] = result[i].split('\t')
+    taxid_acc = {taxid[0]: taxid[1:] for taxid in result}
+    empty = ''
+    if empty in taxid_acc:
+        del taxid_acc[empty]
+    return taxid_acc
 
-#----
+
+
+#----------
+
+#1
+content = modify_string('headers_OG0000000')
+species = get_species(content)
+df = search_pkl_df(species)
+result = main(df) # main(df) calls remove_duplicate_accession()
+empty_df = pd.DataFrame()
+for i in result: # the for-loop merges the results retrieved from multiple processors
+    empty_df = empty_df.append(i, ignore_index = True)
+
+result1 = get_hitproportion_meaneval(empty_df)
+
+#--------------run get taxid
+with Pool(20) as p:
+    res = p.map(get_taxid, edit_acc(list(result1['sseqid'])))
+    Glob = {} # to join the results from the parallel run
+    for i in res:
+        Glob.update(i)
+
+result1['taxid'] = np.nan #add a new col
+for i in range(len(result1['sseqid'])):
+    edited = result1['sseqid'][i].split('.', 1)[0]
+    result1.loc[result1['sseqid'] == result1['sseqid'][i], 'taxid'] = Glob[edited]
+
+unique_taxids = list(set(result1['taxid']))
+with open('all_taxids.txt', 'w') as f:
+    for item in unique_taxids:
+        f.write("%s\n" % item)
+
+#--------------run get taxonomic rank
+
+os.system("split -l 199 all_taxids.txt taxid_part")
+files = list(glob.glob(os.path.join('/home/fardokht/','taxid_part*'))) # a list of file with a specifc name #the dirctory muct be changed
+
+taxid_full_rank = {} # this dict contain taxid as keys and a list of taxonomic division as the values
+for i in files:
+    result = get_taxonomic_rank(i)
+    taxid_full_rank.update(result)
+
+taxid_rank = {}
+for i in taxid_full_rank:
+    if "phylum" in taxid_full_rank[i]:
+        for j in range(len(taxid_full_rank[i])):
+            if taxid_full_rank[i][j] == "phylum":
+                taxid_rank[i] = [taxid_full_rank[i][j], taxid_full_rank[i][j+1]]
+            continue
+    else:
+        taxid_rank[i] = [taxid_full_rank[i][0], taxid_full_rank[i][1]]
+
+# add two more columns 'rank', 'name'
+result1['rank'] = np.nan
+result1['name'] = np.nan
+
+# taxid_rank is a dict {'taxid': ['rank':'name'], ...}
+#the follwoing, iterates through taxid_rank and for equla values with joined_df['taxid'], it adds rank and name to joined_df['rank'] and joined_df['name']
+for i in taxid_rank:
+    result1.loc[result1['taxid'] == int(i), ['rank', 'name']] = [taxid_rank[i][0], taxid_rank[i][1]]
+
+#phylum_only is a list created from ncbi/taxonomoy_browser/bacteria. this list contains the phylumns under the category bacteria
+phylum_only = ['Acidobacteria', 'Aquificae', 'Caldiserica', 'Candidatus Cryosericota', 'Calditrichaeota', 'Chrysiogenetes', 'Coprothermobacterota', 'Deferribacteres', 'Dictyoglomi', 'Elusimicrobia', 'Bacteroidetes', 'Balneolaeota', 'Candidatus Kapabacteria', 'Chlorobi', 'Ignavibacteriae', 'Rhodothermaeota', 'candidate division LCP-89', 'candidate division Zixibacteria', 'Candidatus Cloacimonetes', 'Candidatus Fermentibacteria', 'Candidatus Hydrogenedentes', 'Candidatus Kryptonia', 'Candidatus Latescibacteria', 'Candidatus Marinimicrobia', 'Fibrobacteres', 'Gemmatimonadetes', 'Fusobacteria', 'Krumholzibacteriota', 'Candidatus Tectomicrobia', 'Nitrospinae', 'Nitrospirae', 'Proteobacteria', 'Candidatus Abyssubacteria', 'Candidatus Aureabacteria', 'Candidatus Omnitrophica', 'Chlamydiae', 'Kiritimatiellaeota', 'Lentisphaerae', 'Planctomycetes', 'Verrucomicrobia', 'Spirochaetes', 'Synergistetes', 'Abditibacteriota', 'Actinobacteria', 'Armatimonadetes', 'Candidatus Dormibacteraeota', 'Candidatus Eremiobacteraeota', 'Chloroflexi', 'Candidatus Margulisbacteria', 'Candidatus Melainabacteria', 'Cyanobacteria', 'Deinococcus-Thermus', 'Firmicutes', 'Tenericutes', 'Thermodesulfobacteria', 'Thermotogae']
+
+#now joined_df is sorted based on occurrence descending and evalue ascending:
+#from the sorted df, we will look for elements of phylum_only in joined_df, and for each element, we get the top 3 acc and 2 random acc numbers.
+joined_df = result1.sort_values(['occurrence', 'mean_eval'], ascending=[False, True])
+top_df = result1.groupby('name').head(3) #we get the top 3
+top_df = top_df.dropna() #remove nan values
+
+random = pd.concat([result1, top_df]).drop_duplicates(keep=False)
+
+#the following gets the random elements for each group:
+unique_names =list(pd.unique(random['name'])) #get unique rank names  ##attention, nan values are included and have to be removed
+unique_names = [x for x in unique_names if str(x) != 'nan']
+#unique_names[np.isnan(unique_names)] = 0
+
+selected_random = pd.DataFrame()
+for i in unique_names:
+    if len(random[random['name'] == i]) >= 2:
+        selected_random = selected_random.append(random[random['name'] == i].sample(n=2, replace = False))
+    else:
+        selected_random = selected_random.append(random[random['name'] == i].sample(n=1, replace=False))
+
+# join top_df and selected_random dfs.
+top_df = top_df.append(selected_random)
+candidates_df = pd.DataFrame()
+for i in phylum_only:
+    candidates_df = candidates_df.append(top_df.loc[top_df['name'].str.lower() == i.lower()])
+    candidates_df.to_csv('candidates_df.csv')
 
 
 
-if __name__ == '__main__':
-    #parse_fasta_headers(afile)
-    content = modify_string('headers_OG0000000')
-    search_pkl_df(content)
-    #
-    df = pd.read_csv('run_result.csv')
-    result = main(df)
-    empty_df = pd.DataFrame()
-    for i in result:
-        empty_df = empty_df.append(i, ignore_index = True)
-    empty_df.to_csv("removed_duplicate_acc.csv", sep='\t', index=False)
-    #
-    with Pool(20) as p:
-        result = p.map(get_taxid, edit_acc(list(joined_df['sseqid'])))
-        Glob = {} # to join the results from the parallel run
-        for i in result:
-            Glob.update(i)
-     
 
-    
+
+
+
+
+
+
+
