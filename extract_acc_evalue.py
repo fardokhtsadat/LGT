519644f7e7af53d05285ab4f1cc479d2a1e1bbe1
Fardokhtsadat Mohammadi <fardokht.fm@gmail.com>
2020-09-15 14:31:01 +0200

    Update extract_acc_evalue.py

--- extract_acc_evalue.py
+++ extract_acc_evalue.py
@@ -123,104 +123,91 @@ def get_taxonomic_rank(afile):
         del taxid_acc[empty]
     return taxid_acc
 
+#-----------------------RUN-------------------------#
+if __name__ == '__main__':
+    parse_fasta_headers('OG0000000.fa') #parse_fasta_headers parses the headers from a fasta file. ['OG0000000.fa' contains 555 headers]
+    headers = modify_string('headers_OG0000000') #modify_string() gets a file containg the headers separated by '\n'.
+    df = search_pkl_df(headers)
+    result = main(df) #main(df) removes duplicate sseqids from the df. One qseqid can hit the same sseqid (sequence) multiple times. These duplicated sseqids are removed.
 
+    empty_df = pd.DataFrame()
+    for i in result: # the for-loop merges the results retrieved from multiple processors
+        empty_df = empty_df.append(i, ignore_index = True)
 
-#----------
+    df1 = get_hitproportion_meaneval(empty_df)
 
-#1
-content = modify_string('headers_OG0000000')
-species = get_species(content)
-df = search_pkl_df(species)
-result = main(df) # main(df) calls remove_duplicate_accession()
-empty_df = pd.DataFrame()
-for i in result: # the for-loop merges the results retrieved from multiple processors
-    empty_df = empty_df.append(i, ignore_index = True)
+    with Pool(20) as p:
+        res = p.map(get_taxid, edit_acc(list(df1['sseqid'])))
+        Glob = {} # to join the results from the parallel run
+        for i in res:
+            Glob.update(i)
 
-result1 = get_hitproportion_meaneval(empty_df)
+    df1['taxid'] = np.nan #add a new col
+    for i in range(len(df1['sseqid'])):
+        edited = df1['sseqid'][i].split('.', 1)[0]
+        df1.loc[df1['sseqid'] == df1['sseqid'][i], 'taxid'] = Glob[edited]
 
-#--------------run get taxid
-with Pool(20) as p:
-    res = p.map(get_taxid, edit_acc(list(result1['sseqid'])))
-    Glob = {} # to join the results from the parallel run
-    for i in res:
-        Glob.update(i)
+    unique_taxids = list(set(df1['taxid']))
+    with open('all_taxids.txt', 'w') as f:
+        for item in unique_taxids:
+            f.write("%s\n" % item)
 
-result1['taxid'] = np.nan #add a new col
-for i in range(len(result1['sseqid'])):
-    edited = result1['sseqid'][i].split('.', 1)[0]
-    result1.loc[result1['sseqid'] == result1['sseqid'][i], 'taxid'] = Glob[edited]
+    os.system("split -l 199 all_taxids.txt taxid_part")
+    files = list(glob.glob(os.path.join('/home/fardokht/','taxid_part*'))) # a list of file with a specifc name #the dirctory muct be changed
+    os.system('rm all_taxids.txt')
 
-unique_taxids = list(set(result1['taxid']))
-with open('all_taxids.txt', 'w') as f:
-    for item in unique_taxids:
-        f.write("%s\n" % item)
+    taxid_full_rank = {} # this dict contain taxid as keys and a list of taxonomic division as the values
+    for i in files:
+        taxid_full_rank.update(get_taxonomic_rank(i))
 
-#--------------run get taxonomic rank
+    os.system('rm taxid_part*')
 
-os.system("split -l 199 all_taxids.txt taxid_part")
-files = list(glob.glob(os.path.join('/home/fardokht/','taxid_part*'))) # a list of file with a specifc name #the dirctory muct be changed
+    taxid_rank = {}
+    for i in taxid_full_rank:
+        if "phylum" in taxid_full_rank[i]:
+            for j in range(len(taxid_full_rank[i])):
+                if taxid_full_rank[i][j] == "phylum":
+                    taxid_rank[i] = [taxid_full_rank[i][j], taxid_full_rank[i][j+1]]
+                continue
+        else:
+            taxid_rank[i] = [taxid_full_rank[i][0], taxid_full_rank[i][1]]
 
-taxid_full_rank = {} # this dict contain taxid as keys and a list of taxonomic division as the values
-for i in files:
-    result = get_taxonomic_rank(i)
-    taxid_full_rank.update(result)
+    # add two more columns 'rank', 'name'
+    df1['rank'] = np.nan
+    df1['name'] = np.nan
 
-taxid_rank = {}
-for i in taxid_full_rank:
-    if "phylum" in taxid_full_rank[i]:
-        for j in range(len(taxid_full_rank[i])):
-            if taxid_full_rank[i][j] == "phylum":
-                taxid_rank[i] = [taxid_full_rank[i][j], taxid_full_rank[i][j+1]]
-            continue
-    else:
-        taxid_rank[i] = [taxid_full_rank[i][0], taxid_full_rank[i][1]]
+    # taxid_rank is a dict {'taxid': ['rank':'name'], ...}
+    #the follwoing, iterates through taxid_rank and for equla values with joined_df['taxid'], it adds rank and name to joined_df['rank'] and joined_df['name']
+    for i in taxid_rank:
+        df1.loc[df1['taxid'] == int(i), ['rank', 'name']] = [taxid_rank[i][0], taxid_rank[i][1]]
 
-# add two more columns 'rank', 'name'
-result1['rank'] = np.nan
-result1['name'] = np.nan
+    phylum_only = ['Acidobacteria', 'Aquificae', 'Caldiserica', 'Candidatus Cryosericota', 'Calditrichaeota', 'Chrysiogenetes', 'Coprothermobacterota', 'Deferribacteres', 'Dictyoglomi', 'Elusimicrobia', 'Bacteroidetes', 'Balneolaeota', 'Candidatus Kapabacteria', 'Chlorobi', 'Ignavibacteriae', 'Rhodothermaeota', 'candidate division LCP-89', 'candidate division Zixibacteria', 'Candidatus Cloacimonetes', 'Candidatus Fermentibacteria', 'Candidatus Hydrogenedentes', 'Candidatus Kryptonia', 'Candidatus Latescibacteria', 'Candidatus Marinimicrobia', 'Fibrobacteres', 'Gemmatimonadetes', 'Fusobacteria', 'Krumholzibacteriota', 'Candidatus Tectomicrobia', 'Nitrospinae', 'Nitrospirae', 'Proteobacteria', 'Candidatus Abyssubacteria', 'Candidatus Aureabacteria', 'Candidatus Omnitrophica', 'Chlamydiae', 'Kiritimatiellaeota', 'Lentisphaerae', 'Planctomycetes', 'Verrucomicrobia', 'Spirochaetes', 'Synergistetes', 'Abditibacteriota', 'Actinobacteria', 'Armatimonadetes', 'Candidatus Dormibacteraeota', 'Candidatus Eremiobacteraeota', 'Chloroflexi', 'Candidatus Margulisbacteria', 'Candidatus Melainabacteria', 'Cyanobacteria', 'Deinococcus-Thermus', 'Firmicutes', 'Tenericutes', 'Thermodesulfobacteria', 'Thermotogae']
 
-# taxid_rank is a dict {'taxid': ['rank':'name'], ...}
-#the follwoing, iterates through taxid_rank and for equla values with joined_df['taxid'], it adds rank and name to joined_df['rank'] and joined_df['name']
-for i in taxid_rank:
-    result1.loc[result1['taxid'] == int(i), ['rank', 'name']] = [taxid_rank[i][0], taxid_rank[i][1]]
+    #now joined_df is sorted based on occurrence descending and evalue ascending:
+    #from the sorted df, we will look for elements of phylum_only in joined_df, and for each element, we get the top 3 acc and 2 random acc numbers.
+    joined_df = df1.sort_values(['occurrence', 'mean_eval'], ascending=[False, True])
+    top_df = joined_df.groupby('name').head(3) #we get the top 3
+    top_df = top_df.dropna() #remove nan values
 
-#phylum_only is a list created from ncbi/taxonomoy_browser/bacteria. this list contains the phylumns under the category bacteria
-phylum_only = ['Acidobacteria', 'Aquificae', 'Caldiserica', 'Candidatus Cryosericota', 'Calditrichaeota', 'Chrysiogenetes', 'Coprothermobacterota', 'Deferribacteres', 'Dictyoglomi', 'Elusimicrobia', 'Bacteroidetes', 'Balneolaeota', 'Candidatus Kapabacteria', 'Chlorobi', 'Ignavibacteriae', 'Rhodothermaeota', 'candidate division LCP-89', 'candidate division Zixibacteria', 'Candidatus Cloacimonetes', 'Candidatus Fermentibacteria', 'Candidatus Hydrogenedentes', 'Candidatus Kryptonia', 'Candidatus Latescibacteria', 'Candidatus Marinimicrobia', 'Fibrobacteres', 'Gemmatimonadetes', 'Fusobacteria', 'Krumholzibacteriota', 'Candidatus Tectomicrobia', 'Nitrospinae', 'Nitrospirae', 'Proteobacteria', 'Candidatus Abyssubacteria', 'Candidatus Aureabacteria', 'Candidatus Omnitrophica', 'Chlamydiae', 'Kiritimatiellaeota', 'Lentisphaerae', 'Planctomycetes', 'Verrucomicrobia', 'Spirochaetes', 'Synergistetes', 'Abditibacteriota', 'Actinobacteria', 'Armatimonadetes', 'Candidatus Dormibacteraeota', 'Candidatus Eremiobacteraeota', 'Chloroflexi', 'Candidatus Margulisbacteria', 'Candidatus Melainabacteria', 'Cyanobacteria', 'Deinococcus-Thermus', 'Firmicutes', 'Tenericutes', 'Thermodesulfobacteria', 'Thermotogae']
+    random = pd.concat([joined_df, top_df]).drop_duplicates(keep=False)
+    unique_names =list(pd.unique(random['name'])) #get unique rank names  ##attention, nan values are included and have to be removed
+    unique_names = [x for x in unique_names if str(x) != 'nan']
+    #unique_names[np.isnan(unique_names)] = 0
 
-#now joined_df is sorted based on occurrence descending and evalue ascending:
-#from the sorted df, we will look for elements of phylum_only in joined_df, and for each element, we get the top 3 acc and 2 random acc numbers.
-joined_df = result1.sort_values(['occurrence', 'mean_eval'], ascending=[False, True])
-top_df = result1.groupby('name').head(3) #we get the top 3
-top_df = top_df.dropna() #remove nan values
+    selected_random = pd.DataFrame()
+    for i in unique_names:
+        if len(random[random['name'] == i]) >= 2:
+            selected_random = selected_random.append(random[random['name'] == i].sample(n=2, replace = False))
+        else:
+            selected_random = selected_random.append(random[random['name'] == i].sample(n=1, replace=False))
 
-random = pd.concat([result1, top_df]).drop_duplicates(keep=False)
+    # join top_df and selected_random dfs.
+    top_df = top_df.append(selected_random)
 
-#the following gets the random elements for each group:
-unique_names =list(pd.unique(random['name'])) #get unique rank names  ##attention, nan values are included and have to be removed
-unique_names = [x for x in unique_names if str(x) != 'nan']
-#unique_names[np.isnan(unique_names)] = 0
-
-selected_random = pd.DataFrame()
-for i in unique_names:
-    if len(random[random['name'] == i]) >= 2:
-        selected_random = selected_random.append(random[random['name'] == i].sample(n=2, replace = False))
-    else:
-        selected_random = selected_random.append(random[random['name'] == i].sample(n=1, replace=False))
-
-# join top_df and selected_random dfs.
-top_df = top_df.append(selected_random)
-candidates_df = pd.DataFrame()
-for i in phylum_only:
-    candidates_df = candidates_df.append(top_df.loc[top_df['name'].str.lower() == i.lower()])
-    candidates_df.to_csv('candidates_df.csv')
-
-
-
-
-
-
-
-
+    candidates_df = pd.DataFrame()
+    for i in phylum_only:
+        candidates_df = candidates_df.append(top_df.loc[top_df['name'].str.lower() == i.lower()])
+    #candidates_df.to_csv('candidates_df.csv')
 
 
 
