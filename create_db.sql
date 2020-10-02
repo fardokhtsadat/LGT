137da8c4401f53373f2e2245116e0a74b60e5636
Fardokhtsadat Mohammadi <fardokht.fm@gmail.com>
2020-10-02 16:22:46 +0200

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -1,12 +1,13 @@
 CREATE DATABASE EUK_DB;
 
-CREATE TABLE acc_taxid_taxonomy (
-    id INT NOT NULL AUTO_INCREMENT,
-    accession varchar(64) NOT NULL,
-    taxid INT NOT NULL,
-    taxonomy varchar(800) NOT NULL,
-    PRIMARY KEY (id)
-);
+CREATE TABLE accession_taxid_taxonomy (
+id int(22) NOT NULL AUTO_INCREMENT,
+accession varchar(64) NOT NULL,
+taxid varchar(16) NOT NULL,
+taxonomy varchar(800) NOT NULL,
+primary key (id),
+unique key accession_index (accession) ,
+unique key taxonomy (accession));
 
 
 # IMPORT DATA:
