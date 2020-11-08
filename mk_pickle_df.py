d6a14fd91b64cca3757a22a0967ab57e8245aea2
Fardokhtsadat Mohammadi <fardokht.fm@gmail.com>
2020-11-08 11:22:18 +0100

    Update mk_pickle_df.py

--- mk_pickle_df.py
+++ mk_pickle_df.py
@@ -1,12 +1,12 @@
 ## The following script extracts some information from blast results. The input is the path of the directory of blast results for each 
 ## the ouput is a pickled data frame with three columns of 'qseqid', 'sseqid', 'evalue' for each species.
 
-import pandas as pd, pickle, glob, os
+import pandas as pd, pickle, glob, os, multiprocessing
 from multiprocessing import Pool
 
 def extract_data(directory):
     all_files = []
-    for file in glob.glob(directory): #for all files in the directory
+    for file in glob.glob(directory):
         all_files.append(file)
     full_blast_files = []
     for j in all_files:
@@ -28,20 +28,19 @@ def extract_data(directory):
     final_df = pd.DataFrame({'qseqid': qseqid_all,
                              'sseqid': sseqid_all,
                              'evalue': evalue_all})
-    #print('Data from blast result found in %s directory are retrieved' %Path)
+    print(multiprocessing.current_process()) #prints the worker thread's number
     return final_df
 
 
 def extract_data_main(num_cpu, list_of_dir, output_file_name):
     with Pool(num_cpu) as p:
         res = p.map(extract_data, list_of_dir)
-    merged_df = pd.DataFrame()
+    merged_df = pd.DataFrame()  
     for i in res:
         merged_df = pd.concat([i, merged_df], ignore_index=True)
     merged_df.to_pickle(output_file_name)
     head, tail = os.path.split(output_file_name)
     print('The pickled dataframe is created and can be found in %s.' % head)
     print('The total number of rows in your pickled dataframe is %i.' % len(merged_df.index))
-    
-    
 
+    
