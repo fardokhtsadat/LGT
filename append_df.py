1ff7c936b23bd1017c0ce853c9e5f76812966ea0
Fardokhtsadat Mohammadi <fardokht.fm@gmail.com>
2020-11-25 13:31:32 +0100

    Update append_df.py

--- append_df.py
+++ append_df.py
@@ -5,10 +5,11 @@ import os
 def append_pkl_df(pkl_df ,data_list, col_list):
     pickle_df = pd.read_pickle(pkl_df) #read in an existing pkl df
     for i in data_list:
-        #temp_df = pd.read_csv(i)
         temp_df = pd.read_table(i, header=None)
         temp_df = temp_df.iloc[:,col_list] #subset the columns we want: qseqid, sseqid, evalue
         temp_df.columns = pickle_df.columns
         pickle_df = pickle_df.append(temp_df, ignore_index=True) #appending the new dataframe to the existing pickled df
     pickle_df.to_pickle(pkl_df) #writing out the pickled df. the new okl df will overwrite the previous pkl df
 
+
+    
