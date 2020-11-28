702bc93e4be4129f02cfa3c22ad3a18e448085fc
Fardokhtsadat Mohammadi <fardokht.fm@gmail.com>
2020-11-28 13:00:16 +0100

    Update config_reader.py

--- config_reader.py
+++ config_reader.py
@@ -1,7 +1,7 @@
 import configparser, os
   
 config = configparser.ConfigParser()
-config.read_file(open('/home/users/LGT_diplo/config'))
+config.read_file(open('config'))
 
 if config['create_pkl_df']['Create_df'] == 'yes':
     wd = config['create_pkl_df']['Working_directory'] #the directory the python scripts are
