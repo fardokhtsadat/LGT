5579574b043703186804cdcd497a94c90f8aa14f
Fardokhtsadat Mohammadi <fardokht.fm@gmail.com>
2021-01-09 10:13:39 +0100

    Update config_reader.py

--- config_reader.py
+++ config_reader.py
@@ -57,5 +57,5 @@ if config['data_selection']['Data_selection'] == 'yes':
     from LGT import *
     print('pkg is imported')
     pkl_dataframe = pd.read_pickle(pkl_df_path)
-    #print(pkl_dataframe)
     wrapper(orthogroups, list_of_names, pkl_dataframe, db_password, num_of_top_hits_per_qseqid, number_of_top_hits_per_species, number_of_random_hits, output_dir)
+
